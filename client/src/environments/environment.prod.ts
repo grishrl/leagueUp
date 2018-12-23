fd43ace579c057cf713fcb89e93f3b9409078b03
Lee Grisham <>
2018-12-22 21:55:24 -0600

    updating prod env file

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  contentful: {
+    spaceID: 'r32eicsr5ho2',
+    token: '5467d9d1180f0a28a53c2a9cb0756269a5522db9f4319be10918695eb59e82cd',
+    categoryIDs: {
+      news: '4LLQW3MgwogACQUQ6q2esA',
+      jumbotron: '1XdCWD3XK0i4a8OuSK62c2'
+    }
+  }
 };
