79c86beec3682a5dbcb2d22d4544aa8d97fc13da
Lee Grisham <>
2018-12-22 23:21:17 -0600

    remove host name from the app

--- server.js
+++ server.js
@@ -64,8 +64,8 @@ app.use('/schedule', scheduleRoutes);
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+    console.log(`Server running at on ${port}/`);
 });
 
 // const options = {
