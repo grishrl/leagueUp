4caf54fa899b5a4a26a8dfa0c07ec498dccf5a6e
Lee Grisham <>
2018-12-22 23:49:39 -0600

    fix path join error

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.listen(port, () => {
 //     console.log('https running on ' + httpsPort);
 // });
 
-app.use('/', express.static(path.join(__dirname + './client/dist/client/')));
+app.use('/', express.static(path.join(__dirname, './client/dist/client/')));
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, './client/dist/client/index.html'));
