18790aba54be1e7c569b601f81123dd1f4595141
Lee Grisham <>
2019-01-11 13:02:36 -0600

    uncomment pre-season questionnaire checks

--- client/src/app/questionnaire/questionnaire.component.ts
+++ client/src/app/questionnaire/questionnaire.component.ts
@@ -50,14 +50,14 @@ export class QuestionnaireComponent implements OnInit {
   
 
   checkTeamMates(){
-    // if(this.passedTeam && this.passedTeam.teamMembers){
-    //   if (this.passedTeam.teamMembers.length >= 5) {
-    //     return true;
-    //   }
-    // }
+    if(this.passedTeam && this.passedTeam.teamMembers){
+      if (this.passedTeam.teamMembers.length >= 5) {
+        return true;
+      }
+    }
     
-    // return false;
-    return true;
+    return false;
+    // return true;
   }
 
 
