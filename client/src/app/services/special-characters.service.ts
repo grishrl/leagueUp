ac25961cf367a73a31c8e88ff88d335625dd1251
Lee Grisham <>
2019-01-24 21:04:37 -0600

    remove special character restriction from team name add some logic to replace breaking special characters with a route friendly replacement update the routefriendly / real team name converter to rely on the new breaking character replacers

---
+++ client/src/app/services/special-characters.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpecialCharactersService {
+
+  constructor() { }
+
+  reserveCharacterMap = [
+
+    { "char": "(", "replace": "-open-parentheses-" },
+    { "char": ")", "replace": "-close-parentheses-" },
+    { "char": ";", "replace": "-semicolon-" },
+    { "char": "=", "replace": "-equals-" },
+    { "char": "+", "replace": "-plus-" },
+    { "char": "/", "replace": "-forwardslash-" },
+    { "char": "?", "replace": "-question-" },
+    { "char": "#", "replace": "-number-" },
+    { "char": "[", "replace": "-open-bracket-" },
+    { "char": "]", "replace": "-close-bracket-" },
+    { "char": "%", "replace": "-percent-" },
+    { "char": "^", "replace": "-caret-" },
+    { "char": "`", "replace": "-backtick-" },
+    { "char": "\\", "replace": "-backslash-" },
+    {"char":" ", "replace":"_"}
+
+  ]
+
+  reverse(segment){
+    this.reserveCharacterMap.forEach(char=>{
+
+      let regex =  char.replace ;
+      segment = segment.replace(new RegExp(regex, 'gi'), char.char);
+
+    });
+    return segment;
+  }
+
+  mapIndexOfReplace(replace){
+    let ind = -1;
+    this.reserveCharacterMap.forEach((map, index) => {
+      if (map.replace === replace) {
+        ind = index;
+      }
+    })
+    return ind;
+  }
+
+  replace(char){
+    let ind = this.mapIndexOf(char);
+      if(ind>-1){
+        return this.reserveCharacterMap[ind].replace;
+      }else{
+        return char;
+      }
+  }
+
+  mapIndexOf(char):number{
+    let ind = -1;
+    this.reserveCharacterMap.forEach((map, index)=>{
+      if(map.char === char){
+          ind = index;
+      }
+    })
+    return ind;
+  }
+}
