2235db1164d6be5b0d8955ab65ea4c56bbdb5644
Lee Grisham <>
2019-06-29 11:40:06 +0100

    update the time service to have methods for calling the season info API and dispensing that to callers; allows for removal of hard coding season numbers, dates, and also provides a way to open registrations via admin users

--- client/src/app/services/timeservice.service.ts
+++ client/src/app/services/timeservice.service.ts
@@ -1,23 +1,54 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { HttpServiceService } from './http-service.service';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TimeserviceService {
 
-  constructor() { }
+  constructor(private httpService:HttpServiceService) {
+    this.init();
+  }
 
   weekMS = 1000*60*60*24*7;
 
   returnWeekNumber(){
-    let now = Date.now();
-    let difference = now - environment.seasonStart;
-    return Math.ceil(difference / this.weekMS);
+    if(this.localInfo){
+      let now = Date.now();
+      let difference = now - this.localInfo.data.seasonStartDate;
+      // console.log('now ', now);
+      // console.log('this.localInfo.seasonStartDate ', this.localInfo.data.seasonStartDate);
+      // console.log('difference ',difference);
+      return Math.ceil(difference / this.weekMS);
+    }else{
+      return 0;
+    }
   }
 
-  returnSeasonStart(){
-    return environment.seasonStart;
+  public getSesasonInfoStream: Subject<object> = new Subject();
+
+  //this is to be used as an external spur to action; if a component loses it's values it can call this method which will fire the next observable
+  getSesasonInfo() {
+    //if we don't have cached response data to use we will fire the http request again.
+    if (this.localInfo) {
+      this.getSesasonInfoStream.next(this.localInfo);
+    } else {
+      //firing the http request one more time.
+      this.init();
+    }
+
+  }
+  localInfo = null;
+  init() {
+    let url = '/admin/getSeasonInfo';
+    this.httpService.httpGet(url, []).subscribe(
+      res=>{
+        this.localInfo = res;
+        this.getSesasonInfo();
+      }
+    )
   }
 
 }
