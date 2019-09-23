bd9bebe188c17edc2eac1c4d965597fe01206cd0
Lee Grisham <>
2019-09-23 10:35:32 -0500

    update reporting deck to dynamically decide how many games need to be submitted based on the boX and scores provided updated the maps to filter out maps all ready banned

--- client/src/app/reporting/reporting-deck/reporting-deck.component.ts
+++ client/src/app/reporting/reporting-deck/reporting-deck.component.ts
@@ -4,6 +4,7 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { UtilitiesService } from 'src/app/services/utilities.service';
 import { TeamService } from 'src/app/services/team.service';
 import {merge, forEach as _forEach } from 'lodash';
+import { Match } from 'src/app/classes/match.class';
 
 @Component({
   selector: 'app-reporting-deck',
@@ -13,20 +14,13 @@ import {merge, forEach as _forEach } from 'lodash';
 export class ReportingDeckComponent implements OnInit {
 
   hideButton:boolean=false;
-  recMatch = {
-    away:{logo:null,teamName:''},
-    home: { logo: null, teamName: ''},
-    mapBans: {
-      awayOne: '',
-      homeOne: '',
-      awayTwo: '',
-      homeTwo: ''
-    },
-    other:{},
-    reported:false,
-    replays:{},
-    matchId:''
-  }
+
+
+  recMatch = new Match();
+
+  totalGames = 3;
+  requiredScore:number
+  offScore:number
 
   uploading = false;
 
@@ -60,7 +54,15 @@ export class ReportingDeckComponent implements OnInit {
           console.log(err);
         }
       )
+      if(match.boX){
+        this.totalGames = match.boX;
+
+
+      }
+      this.requiredScore = (this.totalGames + 1) / 2;
+      this.offScore = this.totalGames - this.requiredScore;
     }
+
     if(this.recMatch.other != null && this.recMatch.mapBans != undefined){
       let obj = this.recMatch.other;
       _forEach(obj, (value, key)=>{
@@ -76,10 +78,12 @@ export class ReportingDeckComponent implements OnInit {
 
     if(this.recMatch.reported){
       this.hideButton = true;
-      this.formControlsDisable();
+
 
     }
+
   }
+
   constructor(private scheduleService: ScheduleService, private util: UtilitiesService, public team:TeamService) { }
 
   maps = {
@@ -100,6 +104,18 @@ export class ReportingDeckComponent implements OnInit {
   AlteracPass: 'Alterac Pass'
 };
 
+returnMaps(ban1, ban2, ban3){
+  let selectable = [];
+
+  _forEach(this.maps, (map)=>{
+    if(ban1 != map && ban2 != map && ban3 != map){
+      selectable.push(map);
+    }
+  });
+
+  return selectable;
+}
+
 removeBan(hero, arr){
   let ind = arr.indexOf(hero);
   if(ind!=-1){
@@ -113,12 +129,13 @@ removeBan(hero, arr){
     awayTwo: '',
     homeTwo: ''
   };
+
   games = {};
   showAdd:boolean = true;
-  showReport:boolean = false;
+
   addGame(){
     let keys = Object.keys(this.games);
-    if(keys.length<3){
+    if(keys.length<this.totalGames){
       this.games[(keys.length + 1).toString()] = {
         homeBans: [],
         awayBans: [],
@@ -127,119 +144,14 @@ removeBan(hero, arr){
         tmp: {}
       };
     }
-    if (keys.length >= 1) {
-      this.showReport = true;
-    }
-    if(keys.length>=2){
+
+    if(keys.length>=this.totalGames-1){
       this.showAdd=false;
     }
 
   }
 
-  // depricated will be removed if this goes well
-  // returnFilteredHeroes(game){
-  //   let disArr = [];
-  //   let currentArr = game.value.homeBans.concat(game.value.awayBans);
-  //   let keys = Object.keys(this.heroes);
-  //   keys.forEach(element=>{
-  //     let heroName = this.heroes[element];
-  //     if(heroName == "Missed"){
-  //       disArr.push(this.heroes[element]);
-  //     } else if(currentArr.indexOf(heroName)==-1){
-  //       disArr.push(this.heroes[element]);
-  //     }
-  //   });
-  //   return disArr;
-  // }
 
-  heroes = {
-    "miss":"Missed",
-    "Abat": "Abathur",
-    "Alar": "Alarak",
-    "Alex": "Alexstrasza",
-    "HANA": "Ana",
-    "Anub": "Anub'arak",
-    "Arts": "Artanis",
-    "Arth": "Arthas",
-    "Auri": "Auriel",
-    "Azmo": "Azmodan",
-    "Fire": "Blaze",
-    "Faer": "Brightwing",
-    "Amaz": "Cassia",
-    "Chen": "Chen",
-    "CCho": "Cho",
-    "Chro": "Chromie",
-    "DECK": "Deckard",
-    "Deha": "Dehaka",
-    "Diab": "Diablo",
-    "DVA0": "D.Va",
-    "L90E": "E.T.C.",
-    "Fals": "Falstad",
-    "FENX": "Fenix",
-    "Gall": "Gall",
-    "Garr": "Garrosh",
-    "Tink": "Gazlowe",
-    "Genj": "Genji",
-    "Genn": "Greymane",
-    "Guld": "Gul'dan",
-    "Hanz": "Hanzo",
-    "Illi": "Illidan",
-    "IMP": "Imperius",
-    "Jain": "Jaina",
-    "Crus": "Johanna",
-    "Junk": "Junkrat",
-    "Kael": "Kael'thas",
-    "KelT": "Kel'Thuzad",
-    "Kerr": "Kerrigan",
-    "Monk": "Kharazim",
-    "Leor": "Leoric",
-    "LiLi": "Li Li",
-    "Wiza": "Li-Ming",
-    "Medi": "Lt. Morales",
-    "Luci": "Lucio",
-    "Drya": "Lunara",
-    "Maie": "Maiev",
-    "Malf": "Malfurion",
-    "Malg":"Mal'Ganis",
-    "MALT": "Malthael",
-    "Mdvh": "Medivh",
-    "MEP":"Mephisto",
-    "Mura": "Muradin",
-    "Murk": "Murky",
-    "Witc": "Nazeebo",
-    "Nova": "Nova",
-    "Oprh":"Orphea",
-    "Prob": "Probius",
-    "Ragn": "Ragnaros",
-    "Rayn": "Raynor",
-    "Rehg": "Rehgar",
-    "Rexx": "Rexxar",
-    "Samu": "Samuro",
-    "Sgth": "Sgt. Hammer",
-    "Barb": "Sonya",
-    "Stit": "Stitches",
-    "STUK": "Stukov",
-    "Sylv": "Sylvanas",
-    "Tass": "Tassadar",
-    "Butc": "The Butcher",
-    "Lost": "The Lost Vikings",
-    "Thra": "Thrall",
-    "Tra0": "Tracer",
-    "Tych": "Tychus",
-    "Tyrl": "Tyrael",
-    "Tyrd": "Tyrande",
-    "Uthe": "Uther",
-    "VALE": "Valeera",
-    "Demo": "Valla",
-    "Vari": "Varian",
-    "Whit":"Whitemane",
-    "Necr": "Xul",
-    "YREL": "Yrel",
-    "Zaga": "Zagara",
-    "Zary": "Zarya",
-    "Zera": "Zeratul",
-    "ZULJ": "Zul'jin"
-  }
 
 addBan(hero, arr){
   arr.push(hero);
@@ -250,51 +162,26 @@ resetReplay(game){
   game.value.replay=null;
 }
 
-  formControlsDisable(){
-    this.awayScoreControl.disable();
-    this.homeScoreControl.disable();
-    this.replay1Control.disable();
-    this.replay2Control.disable();
+showReportButton(){
+  let show = true;
+  if(this.recMatch.reported){
+    show = false;
+  }else if(this.uploading){
+    show = false;
+  }else{
+    show = !this.disableSubmit();
   }
-
-  hideReplaySubmit(){
-    if(this.recMatch.replays){
-      return false;
-    }else{
-      return true;
-    }
-
-  }
+  return show;
+}
 
   removeGame(game, games){
     delete games[game];
   }
 
-  awayScoreControl = new FormControl('', [
-    Validators.required
-  ]);
-  homeScoreControl = new FormControl('', [
-    Validators.required
-  ]);
-  replay1Control = new FormControl('', [
-    Validators.required
-  ]);
-  replay2Control = new FormControl('', [
-    Validators.required
-  ]);
-  replay3Control = new FormControl('', [
-    Validators.required
-  ]);
 
-  reportForm = new FormGroup({
-    awayScore: this.awayScoreControl,
-    homeScore: this.homeScoreControl,
-    replay1: this.replay1Control,
-    replay2: this.replay2Control
-  })
 
   ngOnInit() {
-    this.util.markFormGroupTouched(this.reportForm);
+    // this.util.markFormGroupTouched(this.reportForm);
   }
 
   initGetTeamsInfo(teams){
@@ -308,45 +195,43 @@ resetReplay(game){
     )
   }
 
-  homeScore: number
-  awayScore: number
-  replay1:any
-  replay2:any
-  replay3:any
 
   thirdReplayRequired:boolean = false;
 
-  scoreSelected(changed) {
-   if(this.homeScore + this.awayScore > 2){
-     this.thirdReplayRequired = true;
-   }else{
-     this.thirdReplayRequired = false;
-     this.replay3 = null;
-   }
-  }
+
+
 
   scoreError:string ='';
   disableSubmit():boolean{
     let disable = true;
+    let homeScore = 0;
+    let awayScore = 0;
+    _forEach(this.games, (game)=>{
+      if(game['winner'] == 'home'){
+        homeScore +=1;
+      }else if(game['winner']=='away'){
+        awayScore +=1;
+      }
+    });
 
-    if(this.homeScore == 2){
-      if(this.awayScore <= 1){
+    if(homeScore == this.requiredScore){
+      if (awayScore <= this.offScore){
         disable = false;
         this.scoreError = '';
       }else{
         disable = true;
         this.scoreError = 'Invalid Score';
       }
-    }else if(this.homeScore == 1){
-      if(this.awayScore == 2){
+    } else if (homeScore == this.offScore){
+      if (awayScore == this.requiredScore){
         disable = false;
         this.scoreError = '';
       }else{
         disable = true;
         this.scoreError = 'Invalid Score';
       }
-    }else if(this.homeScore == 0){
-      if(this.awayScore == 2){
+    }else if(homeScore == 0){
+      if (awayScore == this.requiredScore){
         disable = false;
         this.scoreError = '';
       }else{
@@ -355,19 +240,19 @@ resetReplay(game){
       }
     }
 
-    if (this.thirdReplayRequired) {
-      if (this.homeScore != null && this.awayScore != null && this.replay1 != null && this.replay2 != null && this.replay3 != null) {
-        disable = false;
-      }else{
-        disable = true;
-      }
-    } else {
-      if (this.homeScore != null && this.awayScore != null && this.replay1 != null && this.replay2 != null) {
-        disable = false;
-      }else{
-        disable = true;
-      }
-    }
+    // if (this.thirdReplayRequired) {
+    //   if (this.homeScore != null && this.awayScore != null && this.replay1 != null && this.replay2 != null && this.replay3 != null) {
+    //     disable = false;
+    //   }else{
+    //     disable = true;
+    //   }
+    // } else {
+    //   if (this.homeScore != null && this.awayScore != null && this.replay1 != null && this.replay2 != null) {
+    //     disable = false;
+    //   }else{
+    //     disable = true;
+    //   }
+    // }
 
     return disable;
   }
@@ -421,6 +306,7 @@ resetReplay(game){
       report['replay'+key.toString()]=game.replay;
 
       let gamenum = key.toString();
+      //hero bans
       // if(game.homeBans.length<3){
       //   alert('Game ' + key + ' home bans is not filled, can not submit.');
       //   submittable = false
@@ -470,7 +356,6 @@ resetReplay(game){
       this.scheduleService.reportMatch(report).subscribe( res=>{
         this.uploading=false;
         this.recMatch.reported = true;
-        this.showReport = false;
       },
       err=>{
         this.uploading = false
@@ -481,3 +366,159 @@ resetReplay(game){
   }
 
 }
+ // depricated will be removed if this goes well
+  // returnFilteredHeroes(game){
+  //   let disArr = [];
+  //   let currentArr = game.value.homeBans.concat(game.value.awayBans);
+  //   let keys = Object.keys(this.heroes);
+  //   keys.forEach(element=>{
+  //     let heroName = this.heroes[element];
+  //     if(heroName == "Missed"){
+  //       disArr.push(this.heroes[element]);
+  //     } else if(currentArr.indexOf(heroName)==-1){
+  //       disArr.push(this.heroes[element]);
+  //     }
+  //   });
+  //   return disArr;
+  // }
+
+  // heroes = {
+  //   "miss":"Missed",
+  //   "Abat": "Abathur",
+  //   "Alar": "Alarak",
+  //   "Alex": "Alexstrasza",
+  //   "HANA": "Ana",
+  //   "Anub": "Anub'arak",
+  //   "Arts": "Artanis",
+  //   "Arth": "Arthas",
+  //   "Auri": "Auriel",
+  //   "Azmo": "Azmodan",
+  //   "Fire": "Blaze",
+  //   "Faer": "Brightwing",
+  //   "Amaz": "Cassia",
+  //   "Chen": "Chen",
+  //   "CCho": "Cho",
+  //   "Chro": "Chromie",
+  //   "DECK": "Deckard",
+  //   "Deha": "Dehaka",
+  //   "Diab": "Diablo",
+  //   "DVA0": "D.Va",
+  //   "L90E": "E.T.C.",
+  //   "Fals": "Falstad",
+  //   "FENX": "Fenix",
+  //   "Gall": "Gall",
+  //   "Garr": "Garrosh",
+  //   "Tink": "Gazlowe",
+  //   "Genj": "Genji",
+  //   "Genn": "Greymane",
+  //   "Guld": "Gul'dan",
+  //   "Hanz": "Hanzo",
+  //   "Illi": "Illidan",
+  //   "IMP": "Imperius",
+  //   "Jain": "Jaina",
+  //   "Crus": "Johanna",
+  //   "Junk": "Junkrat",
+  //   "Kael": "Kael'thas",
+  //   "KelT": "Kel'Thuzad",
+  //   "Kerr": "Kerrigan",
+  //   "Monk": "Kharazim",
+  //   "Leor": "Leoric",
+  //   "LiLi": "Li Li",
+  //   "Wiza": "Li-Ming",
+  //   "Medi": "Lt. Morales",
+  //   "Luci": "Lucio",
+  //   "Drya": "Lunara",
+  //   "Maie": "Maiev",
+  //   "Malf": "Malfurion",
+  //   "Malg":"Mal'Ganis",
+  //   "MALT": "Malthael",
+  //   "Mdvh": "Medivh",
+  //   "MEP":"Mephisto",
+  //   "Mura": "Muradin",
+  //   "Murk": "Murky",
+  //   "Witc": "Nazeebo",
+  //   "Nova": "Nova",
+  //   "Oprh":"Orphea",
+  //   "Prob": "Probius",
+  //   "Ragn": "Ragnaros",
+  //   "Rayn": "Raynor",
+  //   "Rehg": "Rehgar",
+  //   "Rexx": "Rexxar",
+  //   "Samu": "Samuro",
+  //   "Sgth": "Sgt. Hammer",
+  //   "Barb": "Sonya",
+  //   "Stit": "Stitches",
+  //   "STUK": "Stukov",
+  //   "Sylv": "Sylvanas",
+  //   "Tass": "Tassadar",
+  //   "Butc": "The Butcher",
+  //   "Lost": "The Lost Vikings",
+  //   "Thra": "Thrall",
+  //   "Tra0": "Tracer",
+  //   "Tych": "Tychus",
+  //   "Tyrl": "Tyrael",
+  //   "Tyrd": "Tyrande",
+  //   "Uthe": "Uther",
+  //   "VALE": "Valeera",
+  //   "Demo": "Valla",
+  //   "Vari": "Varian",
+  //   "Whit":"Whitemane",
+  //   "Necr": "Xul",
+  //   "YREL": "Yrel",
+  //   "Zaga": "Zagara",
+  //   "Zary": "Zarya",
+  //   "Zera": "Zeratul",
+  //   "ZULJ": "Zul'jin"
+  // }
+
+    // awayScoreControl = new FormControl('', [
+  //   Validators.required
+  // ]);
+  // homeScoreControl = new FormControl('', [
+  //   Validators.required
+  // ]);
+  // replay1Control = new FormControl('', [
+  //   Validators.required
+  // ]);
+  // replay2Control = new FormControl('', [
+  //   Validators.required
+  // ]);
+  // replay3Control = new FormControl('', [
+  //   Validators.required
+  // ]);
+
+  // reportForm = new FormGroup({
+  //   awayScore: this.awayScoreControl,
+  //   homeScore: this.homeScoreControl,
+  //   replay1: this.replay1Control,
+  //   replay2: this.replay2Control
+  // })
+
+    // formControlsDisable(){
+  //   this.awayScoreControl.disable();
+  //   this.homeScoreControl.disable();
+  //   this.replay1Control.disable();
+  //   this.replay2Control.disable();
+  // }
+    // recMatch = {
+  //   away:{logo:null,teamName:''},
+  //   home: { logo: null, teamName: ''},
+  //   mapBans: {
+  //     awayOne: '',
+  //     homeOne: '',
+  //     awayTwo: '',
+  //     homeTwo: ''
+  //   },
+  //   other:{},
+  //   reported:false,
+  //   replays:{},
+  //   matchId:''
+  // }
+
+  // scoreSelected(changed) {
+  // //  if(this.homeScore + this.awayScore > 2){
+
+  // //  }else{
+
+  // //  }
+  // }
