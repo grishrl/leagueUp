a2abd6a8e8c6804146ef8a10ce1f9305c5e68234
Lee Grisham <>
2019-09-30 14:23:23 -0500

    create a hero profile API wrapper that contains API routes and methods in one file

--- server/methods/heroesProfileAPI.js
+++ server/methods/heroesProfileAPI.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const querystring = require('querystring');
 
 const hpAPIbase = 'https://api.heroesprofile.com/api/';
 
@@ -10,6 +11,8 @@ const highestStat = 'NGS/Leaderboard/Highest/Total/Stat?stat={stat}&season={seas
 
 const avgStat = 'NGS/Leaderboard/Highest/Average/Stat?stat={stat}&season={season}';
 
+const playerProfile = '/Player?battletag={btag}&region=1';
+
 const playerHeroStat = 'NGS/Player/Hero/Stat?battletag={btag}&region=1&hero={hero}&season={season}&division={division}';
 
 const ngsPlayerProfile = 'NGS/Player/Profile?battletag={btag}&region=1';
@@ -20,3 +23,104 @@ const ngsPlayerProfileParams = {
 }
 
 const playerAllHero = 'Player/Hero/All?mode=json&battletag={btag}&region=1&api_token={token}&game_type=Storm League';
+
+
+
+async function matchUploadFn(postObj) {
+
+    if (!postObj.hasOwnProperty('api_token')) {
+        postObj['api_token'] = process.env.heroProfileAPIkey;
+    }
+
+    const config = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+        }
+    };
+
+    let url = hpAPIbase + matchUpload;
+
+    let returnUrl = 'null';
+    try {
+        returnUrl = await axios.get(postToHotsProfileURL + '?' + querystring.stringify(postObj), config);
+    } catch (error) {
+        console.log(error);
+    }
+
+    return returnUrl;
+
+};
+
+function playerMmrAPI(battletag) {
+    let url = hpAPIbase + playerMmr;
+
+    url = url.replace('{btag}', encodeURIComponent(battletag));
+
+    url = appendApiToken(url);
+
+    return axios.get(url).then(
+        (reply) => {
+            return reply.data[battletag];
+        },
+        (err) => {
+            throw err;
+        }
+    );
+}
+
+function playerProfileFn(btag) {
+    let url = hpAPIbase + playerProfile;
+    url = url.replace('{btag}', encodeURIComponent(btag));
+    url = appendApiToken(url);
+    return axios.get(url).then(
+        rep => {
+            return rep.data;
+        }, err => {
+            throw err;
+        }
+    );
+}
+
+async function highestStatFn(stat, season) {
+    let url = hpAPIbase + highestStat;
+
+    url = url.replace('{stat}', stat);
+    url = url.replace('{season}', season);
+
+    url = appendApiToken(url);
+
+    const config = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+        }
+    }
+
+    let returnUrl = 'null';
+
+    try {
+        returnUrl = await axios.get(url, config);
+    } catch (error) {
+        console.log(error);
+    }
+
+    return returnUrl;
+
+}
+
+
+function appendApiToken(url) {
+    if (url.indexOf('?') > -1) {
+        url += '&';
+    } else {
+        url += '?';
+    }
+    url += 'api_token=' + process.env.heroProfileAPIkey;
+    return url;
+}
+
+module.exports = {
+    matchUpload: matchUploadFn,
+    playerMmrAPI: playerMmrAPI,
+    playerProfile: playerProfileFn,
+    highestStat: highestStatFn
+};
