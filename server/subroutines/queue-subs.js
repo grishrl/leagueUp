f3d04372191c4f51a4d7ad4a940eda59763134c7
Lee Grisham <>
2019-09-30 18:28:01 -0500

    clean up queue-subs

--- server/subroutines/queue-subs.js
+++ server/subroutines/queue-subs.js
@@ -1,7 +1,3 @@
-/**
- * 
- */
-
 const Admin = require('../models/admin-models');
 const util = require('../utils');
 const logger = require('./sys-logging-subs');
@@ -142,44 +138,11 @@ function addToPendingTeamMemberQueue(teamId, teamLower, userId, user) {
     }
 }
 
-//this method updates any pending member queues that exist when a team name is chagned
-//teamnameOld:string, old teamname
-//teamnameNew:string, new teamname
-// function updatePendingMembersTeamNameChange(teamnameOld, teamnameNew) {
-
-//     //log object
-//     let logObj = {};
-//     logObj.actor = 'SYSTEM SUBROUTINE addToPendingTeamMemberQueue';
-//     logObj.action = ' update pending queue teamname ';
-//     logObj.logLevel = 'STD';
-//     logObj.timeStamp = new Date().getTime();
-
-//     //find all pending queues for the team
-//     Admin.PendingQueue.find({ teamName: teamnameOld }).then((foundQueue) => {
-//         if (foundQueue && foundQueue.length > 0) {
-//             foundQueue.forEach(queue => {
-//                 logObj.target = queue._id;
-//                 queue.teamName = teamnameNew;
-//                 queue.save().then((saved) => {
-//                     logObj.action += ' pending queue team name updated';
-//                     logger(logObj);
-//                 }, (err) => {
-//                     logObj.logLevel = "ERROR";
-//                     logObj.error = err;
-//                     logger(logObj);
-//                 });
-//             })
-//         }
-//     }, (err) => {
-//         console.log('error'); //static logging
-//     })
-// }
 
 module.exports = {
     removePendingQueue: cleanUpPendingQueue,
     removePendingByTeamAndUser: cleanUpPendingQueueTeamnameUsername,
     addToPendingTeamMemberQueue: addToPendingTeamMemberQueue,
     removePendingQueueByUsername: removePendingQueueByUsername,
-    // updatePendingMembersTeamNameChange: updatePendingMembersTeamNameChange,
     removePendingAvatarQueue: cleanUpAvatarPendingQueue
 }
