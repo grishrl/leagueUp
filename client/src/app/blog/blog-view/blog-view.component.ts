e84d0c2fc93b31810439013d9fd124df4c7d64d8
Lee Grisham <>
2020-03-25 21:15:10 -0500

    move blog sanatizer to a run once function; hopefully reduces the "flicker" of posts

--- client/src/app/blog/blog-view/blog-view.component.ts
+++ client/src/app/blog/blog-view/blog-view.component.ts
@@ -9,65 +9,68 @@ import { Post } from 'src/app/services/wordpress.service';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-blog-view',
-  templateUrl: './blog-view.component.html',
-  styleUrls: ['./blog-view.component.css'],
+  selector: "app-blog-view",
+  templateUrl: "./blog-view.component.html",
+  styleUrls: ["./blog-view.component.css"],
   encapsulation: ViewEncapsulation.None
 })
 export class BlogViewComponent implements OnInit {
-
   //component properties
-  recId: string  //local property for a receieved blog ID
-  displayBlog:Post //local property to hold a fetched blog
-  displayAuthor:Author
-  slug:string
+  recId: string; //local property for a receieved blog ID
+  displayBlog: Post; //local property to hold a fetched blog
+  displayAuthor: Author;
+  slug: string;
+  sanatizedBlogContent;
 
-  constructor(private contentfulService:ContentfulService, private route: ActivatedRoute, public md:MarkdownParserService, public blogCommon:BlogCommonService, private WP:WordpressService, public sanitizer:DomSanitizer) {
+  constructor(
+    private contentfulService: ContentfulService,
+    private route: ActivatedRoute,
+    public md: MarkdownParserService,
+    public blogCommon: BlogCommonService,
+    private WP: WordpressService,
+    public sanitizer: DomSanitizer
+  ) {
     //gets the ID from the url route
-    if(this.route.snapshot.params['id']){
-      this.recId = this.route.snapshot.params['id'];
+    if (this.route.snapshot.params["id"]) {
+      this.recId = this.route.snapshot.params["id"];
     }
 
-    this.route.data.subscribe(
-      res => {
-        if(res.blogId){
-          this.recId = res.blogId;
-        }
-        if (res.slug) {
-          this.slug = res.slug;
-        }
+    this.route.data.subscribe(res => {
+      if (res.blogId) {
+        this.recId = res.blogId;
       }
-    )
-   }
-
+      if (res.slug) {
+        this.slug = res.slug;
+      }
+    });
+  }
+  //sanitizer.bypassSecurityTrustHtml(displayBlog.content)
   ngOnInit() {
     //gets provided blog post from received id
     this.displayBlog = new Post();
     this.displayAuthor = new Author();
-    if(this.recId){
-      this.WP.getBlogPosts([{ slug: this.recId }]).subscribe(
-        (res: any) => {
-          this.displayBlog = res.posts[0];
-          this.WP.getCacheAuthor(this.displayBlog.author).subscribe(
-            (auth: Author) => {
-              this.displayAuthor = auth;
-            }
-          )
-        }
-      )
-    }else if(this.slug){
-      this.WP.getBlogPosts([{slug:this.slug}]).subscribe(
-        (res: any) => {
-          this.displayBlog = res.posts[0];
-          this.WP.getCacheAuthor(this.displayBlog.author).subscribe(
-            (auth: Author) => {
-              this.displayAuthor = auth;
-            }
-          )
-        }
-      )
+    if (this.recId) {
+      this.WP.getBlogPosts([{ slug: this.recId }]).subscribe((res: any) => {
+        this.displayBlog = res.posts[0];
+        this.sanatizedBlogContent = this.sanitizer.bypassSecurityTrustHtml(this.displayBlog.content);
+        this.WP.getCacheAuthor(this.displayBlog.author).subscribe(
+          (auth: Author) => {
+            this.displayAuthor = auth;
+          }
+        );
+      });
+    } else if (this.slug) {
+      this.WP.getBlogPosts([{ slug: this.slug }]).subscribe((res: any) => {
+        this.displayBlog = res.posts[0];
+        this.sanatizedBlogContent = this.sanitizer.bypassSecurityTrustHtml(
+          this.displayBlog.content
+        );
+        this.WP.getCacheAuthor(this.displayBlog.author).subscribe(
+          (auth: Author) => {
+            this.displayAuthor = auth;
+          }
+        );
+      });
     }
-
   }
-
 }
