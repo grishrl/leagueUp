ed1d9be5011cdcf14da08b69eb05af40c2519160
Lee Grisham <>
2020-07-25 17:19:55 -0500

    add ability to view past MVPs and paginate them

--- client/src/app/mvp-page/mvp-page.component.ts
+++ client/src/app/mvp-page/mvp-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MvpService } from '../services/mvp.service';
 import { TimeserviceService } from '../services/timeservice.service';
+import { PageEvent } from '@angular/material';
 
 @Component({
   selector: "app-mvp-page",
@@ -10,17 +11,50 @@ import { TimeserviceService } from '../services/timeservice.service';
 export class MvpPageComponent implements OnInit {
   constructor(private mvpService: MvpService, private timeService:TimeserviceService) {
             this.timeService.getSesasonInfo().subscribe((res) => {
-              console.log("res>>", res);
               this.currentSeason = res["value"];
+              this.selectedSeason = res["value"];
+              for (let i = 9; i <= this.currentSeason; i++) {
+                this.seasonDropdown.push(i);
+              }
+              this.initMvps();
             });
   }
 
   currentSeason;
+  selectedSeason;
+
+  seasonDropdown = [];
+
+  displayArray = [];
+
+  length;
+  pageSize = 15;
+
+  pageEventHandler(pageEvent:PageEvent){
+
+    let i = pageEvent.pageIndex * this.pageSize;
+    let endSlice = i + this.pageSize;
+    if (endSlice > this.mvpList.length) {
+      endSlice = this.mvpList.length;
+    }
+    this.displayArray = [];
+    this.displayArray = this.mvpList.slice(i, endSlice);
+
+  }
+
 
   mvpList = [];
   ngOnInit(): void {
-    this.mvpService.getBySeason(this.currentSeason).subscribe(res => {
-      this.mvpList = res;
-    });
+
+
+  }
+
+  initMvps(){
+        this.mvpService.getBySeason(this.selectedSeason).subscribe((res) => {
+          this.mvpList = res;
+          this.length = res.length;
+          let endSlice = this.pageSize > res.length ? res.length : this.pageSize;
+          this.displayArray = res.slice(0, endSlice);
+        });
   }
 }
