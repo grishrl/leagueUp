1ca32dfb6bc11c0af76c491970960a5f65aff0b5
Lee Grisham <>
2020-07-25 17:36:17 -0500

    add past potg capabilities to the potg page

--- client/src/app/potg-page/potg-page.component.ts
+++ client/src/app/potg-page/potg-page.component.ts
@@ -12,7 +12,20 @@ export class PotgPageComponent implements OnInit {
   constructor(
     private mvpServ: MvpService,
     private timeService: TimeserviceService
-  ) {}
+  ) {
+                this.timeService.getSesasonInfo().subscribe((res) => {
+                  this.currentSeason = res["value"];
+                  this.selectedSeason = res["value"];
+                  for (let i = 9; i <= this.currentSeason; i++) {
+                    this.seasonDropdown.push(i);
+                  }
+                  this.initPotgs();
+                });
+  }
+
+  currentSeason;
+  selectedSeason;
+  seasonDropdown = [];
 
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
 
@@ -33,51 +46,32 @@ export class PotgPageComponent implements OnInit {
 
   mvpList = [];
   ngOnInit(): void {
-                     this.timeService.getSesasonInfo().subscribe((res) => {
-                       this.mvpServ
-                         .getMvpById("season", res["value"])
-                         .subscribe((mvps) => {
-                           console.log(mvps);
-                           mvps.forEach((m) => {
-                             if (m.potg_link && m.potg_link.length > 0) {
-                               this.mvpList.push(m);
-                             }
-                           });
-                           this.displayArray = this.mvpList.slice(
-                             0,
-                             this.pageSize
-                           );
-                           this.length = this.mvpList.length;
-                           this.paginator.firstPage();
-                         });
-                     });
+
                    }
+
+  initPotgs(){
+    this.mvpList = [];
+      this.mvpServ
+        .getMvpById("season",this.selectedSeason)
+        .subscribe((mvps) => {
+          console.log(mvps);
+          mvps.forEach((m) => {
+            if (m.potg_link && m.potg_link.length > 0) {
+              this.mvpList.push(m);
+            }
+          });
+          this.displayArray = this.mvpList.slice(
+            0,
+            this.pageSize
+          );
+          this.length = this.mvpList.length;
+          this.paginator.firstPage();
+        });
+
+  }
 
   invert = 1;
   lastChoice;
-  // sort(sortBy){
-  //   console.log('lastChoice', this.lastChoice)
-  //   console.log("sortBy", sortBy);
-  //   if(this.lastChoice == sortBy){
-  //     this.invert = -1*this.invert;
-  //   }else{
-  //     this.invert = 1;
-  //   }
-  //   console.log("invert", this.invert);
-  //   this.lastChoice = sortBy;
-  //   this.mvpList = this.mvpList.sort(
-  //     (a,b)=>{
-  //       let retVal = 0;
-  //       if(a[sortBy] && b[sortBy]){
-  //        retVal = this.invert*(a[sortBy] - b[sortBy]);
-  //       }
-  //       return retVal;
-  //     }
-  //   );
-  //   // if(this.invert<0){
-  //   //   this.mvpList = this.mvpList.reverse();
-  //   // }
-  // }
 
   sort(prop) {
     if (this.lastChoice == prop) {
