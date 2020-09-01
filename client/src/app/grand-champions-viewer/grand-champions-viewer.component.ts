fe1d9c9909c1d77b020e3f91dafbee5ea38291c1
Lee Grisham <>
2020-09-01 09:45:00 -0500

    update to goats to make it neater

--- client/src/app/grand-champions-viewer/grand-champions-viewer.component.ts
+++ client/src/app/grand-champions-viewer/grand-champions-viewer.component.ts
@@ -11,14 +11,20 @@ import { forEach } from 'lodash';
 export class GrandChampionsViewerComponent implements OnInit {
   currentSeason = 0;
   grandFinals = {};
-
+  lastSeason;
+  selectedSeason;
+  seasonArray = [];
+  matches;
   constructor(
     private sched: ScheduleService,
     private timeServ: TimeserviceService
   ) {
     this.timeServ.getSesasonInfo().subscribe((res) => {
       this.currentSeason = res.value;
+      this.lastSeason = this.currentSeason-1;
+      this.selectedSeason = this.lastSeason;
       for (var i = 1; i < this.currentSeason; i++) {
+        this.seasonArray.push(i);
         this.grandFinals[i] = {};
       }
     });
@@ -27,6 +33,24 @@ export class GrandChampionsViewerComponent implements OnInit {
   sortOrder = (a, b) => {
     return a.value.sortOrder > b.value.sortOrder ? 1 : 0;
   };
+
+  next(){
+    if(this.selectedSeason<this.lastSeason){
+      this.selectedSeason++;
+      this.createMatches();
+    }
+  }
+
+  previous(){
+        if (this.selectedSeason > 1) {
+          this.selectedSeason--;
+          this.createMatches();
+        }
+  }
+
+  createMatches(){
+    this.matches = this.grandFinals[this.selectedSeason];
+  }
 
   ngOnInit(): void {
     this.sched.getGrandFinals().subscribe((res) => {
