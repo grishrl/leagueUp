f027d178faee20b2d41d4a7b5883071fd25b3051
Lee Grisham <>
2020-09-11 16:40:12 -0500

    update the map banner image keys for hanamura

--- client/src/app/match/games-information/games-information.component.ts
+++ client/src/app/match/games-information/games-information.component.ts
@@ -8,33 +8,34 @@ import { HeroesProfileService } from 'src/app/services/heroes-profile.service';
 
 
 @Component({
-  selector: 'app-games-information',
-  templateUrl: './games-information.component.html',
-  styleUrls: ['./games-information.component.css']
+  selector: "app-games-information",
+  templateUrl: "./games-information.component.html",
+  styleUrls: ["./games-information.component.css"],
 })
 export class GamesInformationComponent implements OnInit, OnChanges {
-
-  constructor(public util: UtilitiesService, private timeService: TimeserviceService, private hp: HeroesProfileService, private scheduleService:ScheduleService) { }
+  constructor(
+    public util: UtilitiesService,
+    private timeService: TimeserviceService,
+    private hp: HeroesProfileService,
+    private scheduleService: ScheduleService
+  ) {}
 
   @Input() match;
   ngOnInit() {
     this.resultsMap = new Map<string, object>();
-    if (this.match.hasOwnProperty('replays')) {
+    if (this.match.hasOwnProperty("replays")) {
       _forEach(this.match.replays, (value, key) => {
-        if (key != '_id') {
+        if (key != "_id") {
           let item = value;
-          this.hp.getReplay(item.data).subscribe(
-            reply => {
-              if (reply && reply.name) {
-                item['map'] = reply.name;
-              }
-              item['winner'] = this.match['other'][key]['winner'];
-              this.resultsMap.set(key, item);
+          this.hp.getReplay(item.data).subscribe((reply) => {
+            if (reply && reply.name) {
+              item["map"] = reply.name;
             }
-          )
+            item["winner"] = this.match["other"][key]["winner"];
+            this.resultsMap.set(key, item);
+          });
         }
-      })
-
+      });
     }
   }
 
@@ -65,35 +66,50 @@ export class GamesInformationComponent implements OnInit, OnChanges {
   */
 
   mapImage = [
-
-    { "map": "noMatch", "image": "Mal_Ganis_Homescreen.jpg" },
-    { "map": "Infernal Shrines", "image": "shrines-banner.jpg" },
-    { "map": "Volskaya Foundry", "image": "volskaya-banner.jpg" },
-    { "map": "Dragon Shire", "image": "dragon-shire-banner.jpg" },
-    { "map": "Towers of Doom", "image": "towers-of-doom-banner.jpg" },
-    { "map": "Tomb of the Spider Queen", "image": "spider-banner.jpg" },
-    { "map": "Battlefield of Eternity", "image": "boe-banner.jpg" },
-    { "map": "Alterac Pass", "image": "alterac-banner.jpg" },
-    { "map": "Cursed Hollow", "image": "cursed-banner.jpg" },
-    { "map": "Braxis Holdout", "image": "braxis-banner.jpg" },
-    { "map": "Sky Temple", "image": "sky-temple-banner.jpg" }
-  ]
+    { map: "noMatch", image: "Mal_Ganis_Homescreen.jpg" },
+    { map: "Infernal Shrines", image: "shrines-banner.jpg" },
+    { map: "Volskaya Foundry", image: "volskaya-banner.jpg" },
+    { map: "Dragon Shire", image: "dragon-shire-banner.jpg" },
+    { map: "Towers of Doom", image: "towers-of-doom-banner.jpg" },
+    { map: "Tomb of the Spider Queen", image: "spider-banner.jpg" },
+    { map: "Battlefield of Eternity", image: "boe-banner.jpg" },
+    { map: "Alterac Pass", image: "alterac-banner.jpg" },
+    { map: "Cursed Hollow", image: "cursed-banner.jpg" },
+    { map: "Braxis Holdout", image: "braxis-banner.jpg" },
+    { map: "Sky Temple", image: "sky-temple-banner.jpg" },
+    { map: "Hanamura Temple", image: "hanamura-banner.jpg" },
+  ];
 
   getBackground(map) {
     if (map) {
       let img;
-      this.mapImage.forEach(image => {
+      this.mapImage.forEach((image) => {
         if (image.map == map) {
           img = image.image;
         }
       });
       if (img) {
-        return 'https://s3.amazonaws.com/' + environment.s3bucketGeneralImage + '/' + img;
+        return (
+          "https://s3.amazonaws.com/" +
+          environment.s3bucketGeneralImage +
+          "/" +
+          img
+        );
       } else {
-        return 'https://s3.amazonaws.com/' + environment.s3bucketGeneralImage + '/' + this.mapImage[0].image;
+        return (
+          "https://s3.amazonaws.com/" +
+          environment.s3bucketGeneralImage +
+          "/" +
+          this.mapImage[0].image
+        );
       }
     } else {
-      return 'https://s3.amazonaws.com/' + environment.s3bucketGeneralImage + '/' + this.mapImage[0].image;
+      return (
+        "https://s3.amazonaws.com/" +
+        environment.s3bucketGeneralImage +
+        "/" +
+        this.mapImage[0].image
+      );
     }
   }
 
@@ -108,7 +124,7 @@ export class GamesInformationComponent implements OnInit, OnChanges {
     if (match.forfeit) {
       let ret = false;
     } else {
-      if (side == 'home') {
+      if (side == "home") {
         if (match.home.score == 2 && match.away.score == 0) {
           ret = true;
         }
@@ -120,7 +136,4 @@ export class GamesInformationComponent implements OnInit, OnChanges {
     }
     return ret;
   }
-
-
-
 }
