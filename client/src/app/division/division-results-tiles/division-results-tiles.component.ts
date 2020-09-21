2c484ff6861463a07beba1f21e00db84f6e0c738
Lee Grisham <>
2020-09-21 14:25:46 -0500

    update division results so that if you change divisions then results info will update

--- client/src/app/division/division-results-tiles/division-results-tiles.component.ts
+++ client/src/app/division/division-results-tiles/division-results-tiles.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { DivisionService } from 'src/app/services/division.service';
 import { StandingsService } from 'src/app/services/standings.service';
 import { ScheduleService } from 'src/app/services/schedule.service';
 import { TeamService } from 'src/app/services/team.service';
@@ -8,72 +6,67 @@ import { UtilitiesService } from 'src/app/services/utilities.service';
 import { TimeserviceService } from 'src/app/services/timeservice.service';
 
 @Component({
-  selector: 'app-division-results-tiles',
-  templateUrl: './division-results-tiles.component.html',
-  styleUrls: ['./division-results-tiles.component.css']
+  selector: "app-division-results-tiles",
+  templateUrl: "./division-results-tiles.component.html",
+  styleUrls: ["./division-results-tiles.component.css"],
 })
 export class DivisionResultsTilesComponent implements OnInit {
+  constructor(
+    private standingsService: StandingsService,
+    private scheduleService: ScheduleService,
+    public team: TeamService,
+    public util: UtilitiesService,
+    private timeService: TimeserviceService
+  ) {}
 
-  constructor(private divisionService: DivisionService, private standingsService: StandingsService,
-    private scheduleService: ScheduleService, public team: TeamService, public util: UtilitiesService,
-    private timeService:TimeserviceService) {
-
-     }
-     currentSeason;
+  currentSeason;
   divisions: any = [];
   standings: any[] = [];
-
-
-
-  provDiv
+  matches: any[] = [];
+  selectedDivision: any;
+  rounds: number[] = [];
+  selectedRound: number;
+  provDiv;
+  seasonVal;
+  pastSeason = false;
 
   @Input() set division(div) {
     if (div != undefined && div != null) {
       this.provDiv = div;
-
     }
   }
 
-  pastSeason=false;
-
-
-@Input() set divObj(div) {
-  if (div != undefined && div != null) {
-    this.provDiv = div.division;
-    this.currentSeason = div.season;
-    this.pastSeason = div.pastSeason;
-
+  @Input() set divObj(div) {
+    if (div != undefined && div != null) {
+      this.provDiv = div.division;
+      this.currentSeason = div.season;
+      this.pastSeason = div.pastSeason;
+      this.ngOnInit();
+    }
   }
-}
 
-seasonVal
   @Input() set season(val) {
     if (val != undefined && val != null) {
       this.seasonVal = val;
+      this.ngOnInit();
     }
   }
 
   ngOnInit() {
+    this.selectedRound = null;
+    this.matches = [];
+    this.rounds = [];
     if (this.seasonVal) {
       this.currentSeason = this.seasonVal;
       this.rounds = this.util.calculateRounds(this.provDiv);
     } else {
-      this.timeService.getSesasonInfo().subscribe(
-        res => {
-          this.currentSeason = res['value'];
-          this.rounds = this.util.calculateRounds(this.provDiv);
-        }
-      );
+      this.timeService.getSesasonInfo().subscribe((res) => {
+        this.currentSeason = res["value"];
+        this.rounds = this.util.calculateRounds(this.provDiv);
+      });
     }
   }
 
-
-
-  matches: any[] = [];
-  selectedDivision: any
-  rounds: number[] = [];
-
-  selectedRound: number
   getMatches() {
     this.matches = [];
     let div;
@@ -84,72 +77,97 @@ seasonVal
     }
 
     let season = this.currentSeason;
-    this.scheduleService.getScheduleMatches(season, div, this.selectedRound).subscribe(
-      res => {
-        this.matches = res;
-        this.matches = this.matches.filter( match=>{
-          return match.reported;
-        })
-        if(this.seasonVal){
-          this.standingsService.getPastStandings(this.provDiv.divisionConcat, this.currentSeason).subscribe(
-            res => {
-              this.standings = res;
-              this.matches.forEach(match => {
-                this.standings.forEach(standing => {
-                  if (match.home.teamName == standing.teamName) {
-                    match.home['losses'] = standing.losses;
-                    match.home['wins'] = standing.wins;
+    this.scheduleService
+      .getScheduleMatches(season, div, this.selectedRound)
+      .subscribe(
+        (res) => {
+          this.matches = res;
+          this.matches = this.matches.filter((match) => {
+            return match.reported;
+          });
+          if (this.seasonVal) {
+            this.standingsService
+              .getPastStandings(this.provDiv.divisionConcat, this.currentSeason)
+              .subscribe((res) => {
+                this.standings = res;
+                this.matches.forEach(
+                  (match) => {
+                    this.standings.forEach((standing) => {
+                      if (match.home.teamName == standing.teamName) {
+                        match.home["losses"] = standing.losses;
+                        match.home["wins"] = standing.wins;
+                      }
+                      if (match.away.teamName == standing.teamName) {
+                        match.away["losses"] = standing.losses;
+                        match.away["wins"] = standing.wins;
+                      }
+                    });
+                    if (match.scheduledTime) {
+                      if (
+                        match.scheduledTime.startTime != null ||
+                        match.scheduledTime.startTime != undefined
+                      ) {
+                        match["friendlyDate"] = this.util.getDateFromMS(
+                          match.scheduledTime.startTime
+                        );
+                        match["friendlyTime"] = this.util.getTimeFromMS(
+                          match.scheduledTime.startTime
+                        );
+                        match["suffix"] = this.util.getSuffixFromMS(
+                          match.scheduledTime.startTime
+                        );
+                      }
+                    }
+                  },
+                  (err) => {
+                    console.log(err);
                   }
-                  if (match.away.teamName == standing.teamName) {
-                    match.away['losses'] = standing.losses;
-                    match.away['wins'] = standing.wins;
+                );
+              });
+          } else {
+            this.standingsService
+              .getStandings(this.provDiv.divisionConcat)
+              .subscribe((res) => {
+                this.standings = res;
+                this.matches.forEach(
+                  (match) => {
+                    this.standings.forEach((standing) => {
+                      if (match.home.teamName == standing.teamName) {
+                        match.home["losses"] = standing.losses;
+                        match.home["wins"] = standing.wins;
+                      }
+                      if (match.away.teamName == standing.teamName) {
+                        match.away["losses"] = standing.losses;
+                        match.away["wins"] = standing.wins;
+                      }
+                    });
+                    if (match.scheduledTime) {
+                      if (
+                        match.scheduledTime.startTime != null ||
+                        match.scheduledTime.startTime != undefined
+                      ) {
+                        match["friendlyDate"] = this.util.getDateFromMS(
+                          match.scheduledTime.startTime
+                        );
+                        match["friendlyTime"] = this.util.getTimeFromMS(
+                          match.scheduledTime.startTime
+                        );
+                        match["suffix"] = this.util.getSuffixFromMS(
+                          match.scheduledTime.startTime
+                        );
+                      }
+                    }
+                  },
+                  (err) => {
+                    console.log(err);
                   }
-                });
-                if (match.scheduledTime) {
-                  if (match.scheduledTime.startTime != null || match.scheduledTime.startTime != undefined) {
-                    match['friendlyDate'] = this.util.getDateFromMS(match.scheduledTime.startTime);
-                    match['friendlyTime'] = this.util.getTimeFromMS(match.scheduledTime.startTime);
-                    match['suffix'] = this.util.getSuffixFromMS(match.scheduledTime.startTime);
-                  }
-                }
-              },
-                err => {
-                  console.log(err);
-                }
-              )
-            });
-        }else{
-          this.standingsService.getStandings(this.provDiv.divisionConcat).subscribe(
-            res => {
-              this.standings = res;
-              this.matches.forEach(match => {
-                this.standings.forEach(standing => {
-                  if (match.home.teamName == standing.teamName) {
-                    match.home['losses'] = standing.losses;
-                    match.home['wins'] = standing.wins;
-                  }
-                  if (match.away.teamName == standing.teamName) {
-                    match.away['losses'] = standing.losses;
-                    match.away['wins'] = standing.wins;
-                  }
-                });
-                if (match.scheduledTime) {
-                  if (match.scheduledTime.startTime != null || match.scheduledTime.startTime != undefined) {
-                    match['friendlyDate'] = this.util.getDateFromMS(match.scheduledTime.startTime);
-                    match['friendlyTime'] = this.util.getTimeFromMS(match.scheduledTime.startTime);
-                    match['suffix'] = this.util.getSuffixFromMS(match.scheduledTime.startTime);
-                  }
-                }
-              },
-                err => {
-                  console.log(err);
-                }
-              )
-            });
+                );
+              });
+          }
+        },
+        (err) => {
+          console.log(err);
         }
-
-      },
-      err => { console.log(err) }
-    )
+      );
   }
 }
