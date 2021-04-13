2876705418cc97b28c470448757c7ac2d09dc321
Lee Grisham <wraithlinggaming@gmail.com>
2021-04-13 14:35:08 -0500

    update clustering info to match heroku recommendations; remove loading all team images from home page until tab is clicked

--- cluster.js
+++ cluster.js
@@ -1,12 +1,14 @@
 const cluster = require('cluster');
 const worker = require('./worker');
 
+var WORKERS = process.env.WEB_CONCURRENCY || 1;
+
 if (cluster.isMaster) {
 
     //testing with two clusters until proofed in heroku
     worker.init();
 
-    for (var i = 0; i < 2; i++) {
+    for (var i = 0; i < WORKERS; i++) {
         console.log('forking cluster')
         cluster.fork();
     }
