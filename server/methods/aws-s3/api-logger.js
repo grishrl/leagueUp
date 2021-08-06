2b06c52cd6cfb0e35302e28e7f3c7fee33db1495
Lee Grisham <wraithlinggaming@gmail.com>
2021-08-06 12:54:48 -0500

    stub api logging

---
+++ server/methods/aws-s3/api-logger.js
@@ -0,0 +1,14 @@
+const APILogging = function() {
+
+    const APILogging = {};
+
+    APILogging.log = {
+        summary: {},
+        logList: []
+    };
+
+    APILogging.addToLog = function(obj) {}
+
+}
+
+}
