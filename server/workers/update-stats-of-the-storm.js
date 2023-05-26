fc0fe80a0f41e6d41dbdcffb38781d78f3b54392
Lee Grisham <lgrisham@techsoupglobal.org>
2023-05-26 00:09:41 -0500

    console.log, instead of logger

--- server/workers/update-stats-of-the-storm.js
+++ server/workers/update-stats-of-the-storm.js
@@ -3,7 +3,7 @@ const updateStatsOfTheStorm = require('../cron-routines/updateStatsOfTheStorm');
 module.exports = () => {
     updateStatsOfTheStorm(console.log).then(
         success => {
-            logger('Finished updating Stats of the Storm database.');
+            console.log('Finished updating Stats of the Storm database.');
         },
         err => {
             console.log('Error updating Stats of the Storm database', err);
